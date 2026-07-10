dcce664e445767c8db6b73acd20c682790618ce0
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-07-10 18:17:16 +0800

    Fix Compose state delegate imports

--- app/src/main/java/com/abk/kernel/ui/components/AbkLoadingTransition.kt
+++ app/src/main/java/com/abk/kernel/ui/components/AbkLoadingTransition.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
